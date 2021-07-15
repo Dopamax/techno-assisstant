92c73d688caef0dca0e7dfa71f7d77b029a7a476
Dopamax <heisenberg.57.yolo@gmail.com>
2021-07-15 16:02:24 +0100

    configuration de la base des donnée
    config de la base de données addons postgresql de heroku

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('db_techno_assisstant', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
+const connection = new Sequelize('dfge4mcc3gldhs', 'ehedawfmalabrh', '56b04d1cb6a643fcfdfae67b0569d97a2e2b5cb2ca91de158112cab231a804dc', {
+    host: 'ec2-35-171-250-21.compute-1.amazonaws.com',
+    dialect: 'postgres'
 });
 
 module.exports = connection;
