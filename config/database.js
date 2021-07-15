a91f82528c8df802f3c6fdb0e3fefe254772b6ea
Dopamax <heisenberg.57.yolo@gmail.com>
2021-07-15 16:12:17 +0100

    changement de port
    changement de port de la base des donnees

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('dfge4mcc3gldhs', 'ehedawfmalabrh', '56b04d1cb6a643fcfdfae67b0569d97a2e2b5cb2ca91de158112cab231a804dc', {
     host: 'ec2-35-171-250-21.compute-1.amazonaws.com',
+    port:'5432',
     dialect: 'postgres'
 });
 
