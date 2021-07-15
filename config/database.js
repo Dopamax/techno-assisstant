79368fb7a5562e9115dd86f95bee0eaf29175c69
Dopamax <heisenberg.57.yolo@gmail.com>
2021-07-15 16:58:35 +0100

    changement config database
    changement config database

--- config/database.js
+++ config/database.js
@@ -1,12 +1,18 @@
 /*const Sequelize = require('sequelize');
 
-const connection = new Sequelize('dfge4mcc3gldhs', 'ehedawfmalabrh', '56b04d1cb6a643fcfdfae67b0569d97a2e2b5cb2ca91de158112cab231a804dc', {
-    host: 'ec2-35-171-250-21.compute-1.amazonaws.com',
-    port:'5432',
-    dialect: 'postgres'
+const connection = new Sequelize('db_techno_assisstant', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
 });
 
 module.exports = connection;*/
+/*
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize('postgres://ehedawfmalabrh:56b04d1cb6a643fcfdfae67b0569d97a2e2b5cb2ca91de158112cab231a804dc@ec2-35-171-250-21.compute-1.amazonaws.com:5432/dfge4mcc3gldhs');
+
+module.exports = connection;
+*/
 const Sequelize = require('sequelize');
 
 const connection = new Sequelize('dfge4mcc3gldhs', 'ehedawfmalabrh', '56b04d1cb6a643fcfdfae67b0569d97a2e2b5cb2ca91de158112cab231a804dc', {
@@ -19,3 +25,5 @@ const connection = new Sequelize('dfge4mcc3gldhs', 'ehedawfmalabrh', '56b04d1cb6
         }
     }
 });
+
+module.exports = connection;
